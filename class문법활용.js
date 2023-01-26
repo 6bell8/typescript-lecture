3c3437a8ea8df12bc594158bd0829461cef57da0
par jin sung <parkgutime@gmail.com>
2023-01-26 10:44:57 +0900

    class문법활용

---
+++ class문법활용.js
@@ -0,0 +1,18 @@
+// 비슷한 object 만들일 있으면 class 만들어쓰자
+// class 객체지향문법 (오류때문에 주석표시)
+// 부모
+// function 기계(구멍) {
+//   this.q = 구멍;
+//   this.w = "snowball";
+// }
+// 자식
+// let nunu = new 기계("consume");
+// let garen = new 기계("strike");
+//2016 새로나온 신문법 (오류때문에 주석표시)
+// class Hero {
+//   constructor(구멍, 구멍2) {
+//     this.q = 구멍;
+//     this.w = 구멍2;
+//   }
+// }
+// let js = new Hero("prk", "jinsung");
---
+++ prototype문법활용.ts
@@ -0,0 +1,9 @@
+function 기계() {
+  this.q = "strike";
+  this.w = "snowball";
+}
+
+//prototype = 유전자
+// prototype 까지 전달되는 이유는 직접자료를 가지고 있으면 그거 출력, 없으면 부모 유전자까지 뒤져서 나오는 것.
+//let nunu = new 기계();
+기계.prototype.name = "kim";
